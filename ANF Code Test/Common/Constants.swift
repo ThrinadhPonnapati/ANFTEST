b07bf77c51fae4816393f1045fd9d2d11a70ad20
Thrinadh Ponnapti <Thrinadhponnapati.ios@gmail.com>
2021-08-29 16:24:18 -0400

    Constant file and extension for alert added.

---
+++ ANF Code Test/Common/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ANF Code Test
+//
+//  Created by Thrinadh Ponnapati on 8/29/21.
+//
+
+import Foundation
+struct URLConstants{
+    static let exploreDataURL = "https://www.abercrombie.com/anf/nativeapp/qa/codetest/codeTest_exploreData.json"
+}
---
+++ ANF Code Test/Common/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  ANF Code Test
+//
+//  Created by Thrinadh Ponnapati on 8/29/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController{
+    func showAlert(title : String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
