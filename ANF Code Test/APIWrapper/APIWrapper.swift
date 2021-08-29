f2609dd5b8d34fc27b759c9355a9dca34d58dc6f
Thrinadh Ponnapti <Thrinadhponnapati.ios@gmail.com>
2021-08-29 18:01:06 -0400

    fetches the dynamic data from the url.

---
+++ ANF Code Test/APIWrapper/APIWrapper.swift
@@ -0,0 +1,45 @@
+//
+//  APIWrapper.swift
+//  ANF Code Test
+//
+//  Created by Thrinadh Ponnapati on 8/27/21.
+//
+
+import Foundation
+
+class APIWrapper{
+    private init(){
+        
+    }
+    static let shared = APIWrapper()
+    func callAPI(urlString : String, completionHandler: @escaping (ExploreData?, URLResponse?, Error?) -> Void){
+        
+        if let url = URL.init(string: urlString){
+            let task = URLSession.shared.ExploreDataTask(with: url) { exploreData, urlResponse, error in
+                completionHandler(exploreData,urlResponse,error)
+            }
+            task.resume()
+        }
+    }
+}
+
+
+// MARK: - URLSession response handlers
+
+extension URLSession {
+    fileprivate func codableTask<T: Codable>(with url: URL, completionHandler: @escaping (T?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return self.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completionHandler(nil, response, error)
+                return
+            }
+            completionHandler(try? newJSONDecoder().decode(T.self, from: data), response, nil)
+        }
+    }
+
+    func ExploreDataTask(with url: URL, completionHandler: @escaping (ExploreData?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return self.codableTask(with: url, completionHandler: completionHandler)
+    }
+}
+
+
